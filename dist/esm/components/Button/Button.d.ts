a375e837b1e1383d0c8104f835fc140fc4d7bc2c
Johnny <xohnny1337@gmail.com>
2022-09-29 12:09:48 +0200

    deleted dist folder

--- dist/esm/components/Button/Button.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import type { ButtonProps } from "./Button.types";
-declare const Button: (props: ButtonProps) => JSX.Element;
-export default Button;
--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
-    label: string;
-}
-
-declare const Button: (props: ButtonProps) => JSX.Element;
-
-export { Button };
